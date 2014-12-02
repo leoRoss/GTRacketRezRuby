2a1b402be7a4cbca3cd5bac0a92e6ad1ec0e185a
Leo Rossignac-Milon <leo.milon@gmail.com>
2014-12-01 20:59:19 -0500

    validation bitches

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,4 +8,27 @@ class Reservation < ActiveRecord::Base
   validates  :duration, presence: true
   validates  :court, presence: true
   validates  :user_id, presence: true
+  validates  :phone, presence: true
+  validates  :name, presence: true
+  before_save :rule_check
+
+  def rule_check 
+  	count = 0
+  	@reservations = Reservation.all
+
+  	@reservations.each do |rez| 
+  		if self.phone == rez.phone && self.start.to_date == rez.start.to_date
+        Error.add(:reservation, "Daily Limit Surpased")
+  		end
+      if self.court == rez.court
+    		if self.start < rez.start+rez.duration.minutes && self.start > rez.start || self.start + self.duration.minutes < rez.start+rez.duration.minutes && self.start + self.duration.minutes > rez.start 
+    		 	Error.add(:reservation, "Time Conflict")
+    		end
+        if self.start < rez.start && self.start+self.duration.minutes > rez.start + rez.duration.minutes
+          Error.add(:reservation, "Time Conflict")
+        end
+      end
+  	end
+  end
+
 end
